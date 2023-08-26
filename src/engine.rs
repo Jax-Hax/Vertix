21437d07a8181f9578cfda878368dd39c6933e88
Jax-Hax <jaxhacks543@gmail.com>
2023-08-25 22:15:28 -0400

    cleaned up renderer, about to improve camera

--- src/engine.rs
+++ src/engine.rs
@@ -2,7 +2,7 @@ use std::iter;
 
 use cgmath::prelude::*;
 use wgpu::util::DeviceExt;
-use winit::{event::*, window::Window};
+use winit::{event::*, window::Window, event_loop::EventLoop};
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
@@ -238,7 +238,41 @@ pub struct State {
 }
 
 impl State {
-    pub async fn new(window: Window) -> Self {
+    pub async fn new() -> (Self,EventLoop<()>) {
+        cfg_if::cfg_if! {
+            if #[cfg(target_arch = "wasm32")] {
+                std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+                console_log::init_with_level(log::Level::Warn).expect("Could't initialize logger");
+            } else {
+                env_logger::init();
+            }
+        }
+    
+        let event_loop = EventLoop::new();
+        let title = env!("CARGO_PKG_NAME");
+        let window = winit::window::WindowBuilder::new()
+            .with_title(title)
+            .build(&event_loop)
+            .unwrap();
+    
+        #[cfg(target_arch = "wasm32")]
+        {
+            // Winit prevents sizing with CSS, so we have to set
+            // the size manually when on web.
+            use winit::dpi::PhysicalSize;
+            window.set_inner_size(PhysicalSize::new(450, 400));
+    
+            use winit::platform::web::WindowExtWebSys;
+            web_sys::window()
+                .and_then(|win| win.document())
+                .and_then(|doc| {
+                    let dst = doc.get_element_by_id("wasm-example")?;
+                    let canvas = web_sys::Element::from(window.canvas());
+                    dst.append_child(&canvas).ok()?;
+                    Some(())
+                })
+                .expect("Couldn't append canvas to document body.");
+        }
         let size = window.inner_size();
 
         // The instance is a handle to our GPU
@@ -440,7 +474,7 @@ impl State {
             multiview: None,
         });
 
-        Self {
+        (Self {
             surface,
             device,
             queue,
@@ -455,10 +489,9 @@ impl State {
             depth_texture,
             window,
             texture_bind_group_layout,
-            entities,
-        }
+            entities
+        },event_loop)
     }
-
     pub fn window(&self) -> &Window {
         &self.window
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,16 @@
-use winit::{event_loop::{EventLoop, ControlFlow}, event::{WindowEvent, VirtualKeyCode, ElementState, Event, KeyboardInput}};
+use winit::{event_loop::ControlFlow, event::{WindowEvent, VirtualKeyCode, ElementState, Event, KeyboardInput}};
 use cgmath::prelude::*;
-use crate::render::{State, Instance};
+use crate::engine::{State, Instance};
 
-mod render;
+mod engine;
 mod model;
 mod resources;
 mod texture;
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Warn).expect("Could't initialize logger");
-        } else {
-            env_logger::init();
-        }
-    }
-
-    let event_loop = EventLoop::new();
-    let title = env!("CARGO_PKG_NAME");
-    let window = winit::window::WindowBuilder::new()
-        .with_title(title)
-        .build(&event_loop)
-        .unwrap();
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        // Winit prevents sizing with CSS, so we have to set
-        // the size manually when on web.
-        use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(450, 400));
-
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-example")?;
-                let canvas = web_sys::Element::from(window.canvas());
-                dst.append_child(&canvas).ok()?;
-                Some(())
-            })
-            .expect("Couldn't append canvas to document body.");
-    }
 
     // State::new uses async code, so we're going to wait for it to finish
-    let mut state = State::new(window).await;
+    let (mut state,event_loop) = State::new().await;
 
     //add models
     const SPACE_BETWEEN: f32 = 3.0;
