95fe69bcdd21f783e50299630f5f4d8ee0fa15a8
Jax-Hax <jaxhacks543@gmail.com>
2023-10-01 16:18:59 -0400

    trying to seperate itno another function but i can't figure out how to add renderpass

--- src/render.rs
+++ src/render.rs
@@ -1,6 +1,14 @@
 use std::iter;
 
-use crate::{state::State, prelude::{InstanceContainer, WorldSpace}, structs::{MeshType, ScreenSpace}, model::DrawModel};
+use hecs::World;
+use wgpu::RenderPass;
+
+use crate::{
+    model::DrawModel,
+    prelude::{InstanceContainer, WorldSpace},
+    state::State,
+    structs::{MeshType, ScreenSpace},
+};
 
 pub fn render(state: &mut State) -> Result<(), wgpu::SurfaceError> {
     let output = state.window.surface.get_current_texture()?;
@@ -41,46 +49,17 @@ pub fn render(state: &mut State) -> Result<(), wgpu::SurfaceError> {
         });
         render_pass.set_pipeline(&state.render_pipeline);
         render_pass.set_bind_group(1, &state.camera.bind_group, &[]);
-        state.queue
+        state
+            .queue
             .write_buffer(&state.uniform_buffer, 0, bytemuck::cast_slice(&[1]));
         render_pass.set_bind_group(2, &state.world_space_bind_group, &[]);
-        for (_entity, (game_object, _ )) in state.world.query_mut::<(&InstanceContainer,&WorldSpace)>() {
-            render_pass.set_vertex_buffer(1, game_object.buffer.slice(..));
-            match &game_object.mesh_type {
-                MeshType::Model(model) => {
-                    render_pass.draw_model_instanced(&model, 0..game_object.length);
-                }
-                MeshType::SingleMesh(mesh) => {
-                    render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
-                    render_pass.set_index_buffer(
-                        mesh.index_buffer.slice(..),
-                        wgpu::IndexFormat::Uint32,
-                    );
-                    render_pass.set_bind_group(0, &mesh.material.bind_group, &[]);
-                    render_pass.draw_indexed(0..mesh.num_elements, 0, 0..1);
-                }
-            }
-        }
-        state.queue
+        let world = &mut state.world;
+        world_space(world);
+        state
+            .queue
             .write_buffer(&state.uniform_buffer, 0, bytemuck::cast_slice(&[0]));
         render_pass.set_bind_group(2, &state.world_space_bind_group, &[]);
-        for (_entity, (game_object, _ )) in state.world.query_mut::<(&InstanceContainer,&ScreenSpace)>() {
-            render_pass.set_vertex_buffer(1, game_object.buffer.slice(..));
-            match &game_object.mesh_type {
-                MeshType::Model(model) => {
-                    render_pass.draw_model_instanced(&model, 0..game_object.length);
-                }
-                MeshType::SingleMesh(mesh) => {
-                    render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
-                    render_pass.set_index_buffer(
-                        mesh.index_buffer.slice(..),
-                        wgpu::IndexFormat::Uint32,
-                    );
-                    render_pass.set_bind_group(0, &mesh.material.bind_group, &[]);
-                    render_pass.draw_indexed(0..mesh.num_elements, 0, 0..1);
-                }
-            }
-        }
+        screen_space(world);
     }
 
     state.queue.submit(iter::once(encoder.finish()));
@@ -88,22 +67,37 @@ pub fn render(state: &mut State) -> Result<(), wgpu::SurfaceError> {
 
     Ok(())
 }
-fn test(state: &mut State) {
-    for (_entity, (game_object, _ )) in state.world.query_mut::<(&InstanceContainer,&WorldSpace)>() {
-        /*render_pass.set_vertex_buffer(1, game_object.buffer.slice(..));
+fn world_space(world: &mut World) {
+    for (_entity, (game_object, _)) in world.query_mut::<(&InstanceContainer, &WorldSpace)>() {
+        render_pass.set_vertex_buffer(1, game_object.buffer.slice(..));
         match &game_object.mesh_type {
             MeshType::Model(model) => {
                 render_pass.draw_model_instanced(&model, 0..game_object.length);
             }
             MeshType::SingleMesh(mesh) => {
                 render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
-                render_pass.set_index_buffer(
-                    mesh.index_buffer.slice(..),
-                    wgpu::IndexFormat::Uint32,
-                );
+                render_pass
+                    .set_index_buffer(mesh.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
                 render_pass.set_bind_group(0, &mesh.material.bind_group, &[]);
                 render_pass.draw_indexed(0..mesh.num_elements, 0, 0..1);
             }
-        }*/
+        }
+    }
+}
+fn screen_space(world: &mut World) {
+    for (_entity, (game_object, _)) in world.query_mut::<(&InstanceContainer, &ScreenSpace)>() {
+        render_pass.set_vertex_buffer(1, game_object.buffer.slice(..));
+        match &game_object.mesh_type {
+            MeshType::Model(model) => {
+                render_pass.draw_model_instanced(&model, 0..game_object.length);
+            }
+            MeshType::SingleMesh(mesh) => {
+                render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
+                render_pass
+                    .set_index_buffer(mesh.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
+                render_pass.set_bind_group(0, &mesh.material.bind_group, &[]);
+                render_pass.draw_indexed(0..mesh.num_elements, 0, 0..1);
+            }
+        }
     }
 }
