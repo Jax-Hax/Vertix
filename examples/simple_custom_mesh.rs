140e96296b94f658ce6b6246ba8c4ae271835d9a
Jax-Hax <jaxhacks543@gmail.com>
2023-10-05 16:15:10 -0400

    fixed simple custom mesh ex

--- examples/simple_custom_mesh.rs
+++ examples/simple_custom_mesh.rs
@@ -1,6 +1,5 @@
-use glam::{Vec3, Quat, Vec2};
-use hecs::World;
-use vertix::{prelude::*, camera::{Camera, default_3d_cam}, primitives::rect, structs::InstanceRaw};
+use glam::{Vec3, Vec2};
+use vertix::{prelude::*, camera::{Camera, default_3d_cam}, primitives::rect};
 fn main() {
     pollster::block_on(run());
 }
@@ -11,25 +10,17 @@ pub async fn run() {
     let (mut state, event_loop) = State::new(true, env!("OUT_DIR"), camera, 5.0, 2.0).await;
     //custom mesh
     let (vertices, indices) = rect(Vec2::new(0.5,0.5), Vec2::new(-0.5,-0.5));
-    state.world.spawn((Instance {is_world_space: false, ..Default::default()},));
-    let instances = update_instances(&mut state.world);
-    let container = state.build_mesh(
+    let mut instance = Instance {is_world_space: false, ..Default::default()};
+    let mut instances = vec![];
+    instances.push(&mut instance);
+    state.build_mesh(
         vertices,
         indices,
         instances,
         state.compile_material("cube-diffuse.jpg").await,
         false,
     );
-    state.world.spawn((container,));
+    state.world.spawn((instance,));
     //render loop
     run_event_loop(state, event_loop, None, None, Some(default_3d_cam));
-}
-fn update_instances(world: &mut World) -> Vec<InstanceRaw>{
-    let mut instances = vec![];
-    for (_entity, (game_object,)) in world
-        .query_mut::<(&mut Instance,)>()
-    {
-        instances.push(game_object.to_raw());
-    }
-    instances
 }
