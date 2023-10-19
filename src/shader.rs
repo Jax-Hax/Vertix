627dcb6c4a10e1b21bb1eb48e9f3ddb8e1c0301d
Jax-Hax <jaxhacks543@gmail.com>
2023-10-19 17:53:02 -0400

    fixed bug

--- src/shader.rs
+++ src/shader.rs
@@ -1,6 +1,6 @@
 use wgpu::{RenderPipeline, Device, SurfaceConfiguration};
 
-use crate::{structs::InstanceRaw, texture::Texture, prelude::Vertex};
+use crate::{texture::Texture, prelude::{Vertex, InstanceRaw}};
 
 pub fn make_shader(shader: &str, device: &Device, render_pipeline_layout:wgpu::PipelineLayout, config: &SurfaceConfiguration) -> RenderPipeline{
     let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
