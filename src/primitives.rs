46c7d3b8994834484cd029e914a12a7e717c64df
Jax-Hax <jaxhacks543@gmail.com>
2023-10-27 13:47:34 -0400

    added fixed primitive

--- src/primitives.rs
+++ src/primitives.rs
@@ -4,10 +4,10 @@ use crate::prelude::Vertex;
 
 pub fn rect(p1: Vec2, p2: Vec2) -> (Vec<Vertex>, Vec<u32>) {
     let vertices = vec![
-        Vertex::new([p1.x,p1.y, 0.0], [0.0,0.0]),
-        Vertex::new([p1.x, p2.y, 0.0],[0.5,0.0]),
-        Vertex::new([p2.x, p1.y, 0.0],[0.0,0.5]),
-        Vertex::new([p2.x,p2.y, 0.0],[1.0, 1.0]),
+        Vertex::new([p1.x,p1.y, 0.0], [0.0,1.0]),
+        Vertex::new([p1.x, p2.y, 0.0],[0.0,0.0]),
+        Vertex::new([p2.x, p1.y, 0.0],[1.0,1.0]),
+        Vertex::new([p2.x,p2.y, 0.0],[1.0, 0.0]),
     ];
 
     let indices = vec![2,1,0, 1, 2, 3];
